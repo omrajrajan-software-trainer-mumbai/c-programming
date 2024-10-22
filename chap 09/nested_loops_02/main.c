b1e435f748bbb742740c067675a5d3674a28fcdb
Omraj Rajan <omrajrajan@gmail.com>
2024-10-22 13:49:28 +0530

    updated and added new projects
    Creating a nested loop in C with CodeBlocks

--- chap 09/nested_loops_02/main.c
+++ chap 09/nested_loops_02/main.c
@@ -5,25 +5,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//The code starts with the main()
+//The Application starts with the main() function
 int main() {
     printf("Creating a nested loop in C with CodeBlocks\n\n");
 
     //Variable declaration and initialization
-    int alphabet, code;
-    char starting_alphabet = 'A';
-    char ending_alphabet = 'Z';
-    int starting_row = 1;
-    int ending_row = 9;
+    int inner_loop_counter, outer_loop_counter;
+    int starting_value = 1;
+    int ending_value = 5;
 
     // Using for loop
-    for(alphabet = starting_alphabet; alphabet <= ending_alphabet; alphabet++)
+    for(outer_loop_counter = starting_value; outer_loop_counter <= ending_value; outer_loop_counter++)
     {
         // A for loop within a for loop
-        for(code = starting_row; code <= ending_row; code++)
+        for(inner_loop_counter = starting_value; inner_loop_counter <= ending_value; inner_loop_counter++)
         {
             //Printing the output
-            printf("%c%d \t", alphabet, code);
+            printf("*");
         }
         // End a line of text
         putchar('\n');
