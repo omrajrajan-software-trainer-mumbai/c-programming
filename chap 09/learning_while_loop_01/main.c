fcfb5436b3435f0967b7663fb299a78ed733dac0
Omraj Rajan <omrajrajan@gmail.com>
2024-10-22 13:20:06 +0530

    added new project "learning_while_loop_01"
    Printing the same statement 10 times using while loop in C with CodeBlocks

---
+++ chap 09/learning_while_loop_01/main.c
@@ -0,0 +1,25 @@
+// Printing the same statement 10 times using while loop in C with CodeBlocks
+// chap - 09 page 121
+
+// Preprocessor directives
+#include <stdio.h>
+#include <stdlib.h>
+
+//The code starts with the main()
+int main() {
+    printf("Printing the same statement 10 times using while loop in C with CodeBlocks\n\n");
+
+    //Variable declaration and initialization
+    int counter;
+    int start_number = 0;
+    int end_number = 10;
+
+    counter = start_number;
+    while(counter < end_number)
+    {
+        // Printing the statement
+        printf("Sore shoulder surgery.\n");
+        counter++;
+    }
+    return 0;
+}
