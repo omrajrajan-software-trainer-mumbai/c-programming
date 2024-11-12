6752fd57b9dfdb1c2c9b237e75bda1c9515b9416
Omraj Rajan <omrajrajan@gmail.com>
2024-11-12 11:27:31 +0530

    added new project - 01_simple_interest
    added new folders, projects and code to the practicals folder in Alok Folder

---
+++ Alok/practicals/prac_01/01_simple_interest/main.c
@@ -0,0 +1,25 @@
+// Program to calculate simple interest taking principal, rate of interest and number of years
+// as input from user in C with CodeBlocks
+// chap - 10
+
+// Preprocessor directives
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Program to calculate simple interest taking principal, rate of interest and number of years as input from user in C with CodeBlocks\n");
+
+    // read principal as 'principal_float'
+    // Let principal_float = 10000, time_int = 2 and rate_of_interest_float = 10
+
+    // read time as 'time_int'
+
+    // read rate_of_interest_float as 'rate_of_interest_float'
+
+    // calculate simple_interest_float using the formula SI = ( principal_float * time_int * rate_of_interest_float ) / 100
+
+    // display SI using 'simple_interest_float' - expected output = 2000
+
+    return 0;
+}
