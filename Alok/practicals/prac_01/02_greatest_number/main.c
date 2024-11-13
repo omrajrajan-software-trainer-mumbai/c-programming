b2ed7b5944b25b6cead94bc994f9fb6b8faff9a7
alok-yadav-programmer-mumbai <aloky3974@gmail.com>
2024-11-13 11:11:18 +0530

    added and updated projects in practicals chapter 10
    Completed greatest number and leap year  project till comment level

---
+++ Alok/practicals/prac_01/02_greatest_number/main.c
@@ -0,0 +1,22 @@
+// Program to find the greatest of three numbers using conditional operator in C with CodeBlocks
+
+// Preprocessor directives
+#include <stdio.h>
+
+int main()
+{
+    printf("Program to find the greatest of three numbers using conditional operator in C with CodeBlocks\n\n");
+
+    // Initialize three numbers let say a, b, c
+
+    // Check if a is greater than b
+    // If true then check if a is greater than c
+        // If true then print 'a' as largest number
+        // If false then print 'c' as largest number
+
+    // If false then check if b is greater than c
+        // If true then print 'b' as largest number
+        // If false then print 'c' as largest number
+
+    return 0;
+}
---
+++ Alok/practicals/prac_01/03_leap_year/main.c
@@ -0,0 +1,23 @@
+// Program to check if the year entered is leap year or not in C with CodeBlocks
+
+// Preprocessor directives
+#include <stdio.h>
+
+int main()
+{
+    printf("Program to check if the year entered is leap year or not in C with CodeBlocks\n\n");
+
+    // Enter a year
+
+    //check the number perfectly divisible by 400
+        // If it's true then print year is a leap year
+
+    //check the number perfectly divisible by 100
+        // If it's true then print year is not a leap year
+
+    //check the number perfectly divisible by 4
+        // If it's true then print year is leap year
+
+
+    return 0;
+}
