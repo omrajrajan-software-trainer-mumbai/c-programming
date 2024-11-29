fd99e49f19fe0bc5e4c6e89e0813b368e02875ce
Omraj Rajan <omrajrajan@gmail.com>
2024-11-29 17:41:32 +0530

    updates to simple_function project in chapter 10
    updates to simple_function project in chapter 10

--- chap 10/simple_function_01/main.c
+++ chap 10/simple_function_01/main.c
@@ -6,18 +6,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Function greet has no parameters and no return type
-void greet()
-{
-    //Prints greeting message
-    printf("Hello World!!!\n");
-}
+// the declaration / prototype of the function greet() has no parameters and no return type
+void greet();
 
 int main()
 {
     printf("Creating a function with no return type and no parameters in C with CodeBlocks\n\n");
 
-    // Calls greet function
+    // Calls greet() function
     greet();
     return 0;
+}
+
+// the definition of the function greet() has no parameters and no return type
+void greet()
+{
+    //Prints greeting message
+    printf("Hello World!!!\n");
 }
